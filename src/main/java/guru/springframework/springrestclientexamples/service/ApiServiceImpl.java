575a79809d118da6236423bcabbc5ebcfe21e484
todd <toddtran9@gmail.com>
2018-06-01 14:13:12 -0700

    implementing functionality to grab users from the sample API.

---
+++ src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
@@ -0,0 +1,27 @@
+package guru.springframework.springrestclientexamples.service;
+
+import guru.springframework.api.domain.User;
+import guru.springframework.api.domain.UserData;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class ApiServiceImpl implements ApiService {
+
+    private RestTemplate restTemplate;
+
+    /**
+     * this constructor being here will cause Spring to autowire in a RestTemplate for us.
+     */
+    public ApiServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public List<User> getUsers(Integer limit) {
+        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        return userData.getData();
+    }
+}
